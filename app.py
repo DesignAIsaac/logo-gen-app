c962fd1734a5ec40fbf0f5369a354a9145d07dbb
Isaac Bonilla <isaacbonilla@Isaacs-MacBook-Pro-2.local>
2025-03-08 02:43:57 -0500

    Updated MongoDB connection string and endpoints

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # Database setup
-MONGO_URI = "mongodb://admin:strongpassword@localhost:27017/logogen?authSource=admin"
+MONGO_URI = "mongodb+srv://admin-logo:xZrc04zgK@cluster0.xvu0t.mongodb.net/logogen?retryWrites=true&w=majority"
 client = MongoClient(MONGO_URI)
 db = client.logogen
 users_collection = db.users
